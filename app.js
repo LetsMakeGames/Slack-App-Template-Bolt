1ac19dc385587aee180d3f80841fe4d24613400f
William Barnes <jakebarnes1989@gmail.com>
2021-10-16 11:32:14 -0400

    Init Commit
    - Setup basic app scaffolding for a Slack App in Bolt. Code should be enough to start an app
    - Includes dotenv for easy environment variable management
    - Includes postman-request for simple HTTP requests (Looks like Bolt may also includes Axios)

---
+++ app.js
@@ -0,0 +1,18 @@
+require('dotenv').config()
+const { App } = require('@slack/bolt');
+
+// Initializes your app with your bot token and signing secret. 
+// Update values in dotenv-example and rename to .env or set your own.
+const app = new App({
+    token: process.env.SLACK_BOT_TOKEN,
+    signingSecret: process.env.SLACK_SIGNING_SECRET
+  });
+
+  /* Enter your code logic here. Get started with Bolt: https://slack.dev/bolt-js/tutorial/getting-started-http */
+
+  (async () => {
+    // Start your app
+    await app.start(process.env.PORT || 3000);
+  
+    console.log('⚡️ Bolt app is running!');
+  })();
