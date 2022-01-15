6c94ec3b2b7c801b1ba6cbd786a48bbd85881767
William Barnes <jakebarnes1989@gmail.com>
2022-01-15 03:25:41 +0000

    Add support for a lovebot slash command

--- app.js
+++ app.js
@@ -8,11 +8,33 @@ const app = new App({
     signingSecret: process.env.SLACK_SIGNING_SECRET
   });
 
-  /* Enter your code logic here. Get started with Bolt: https://slack.dev/bolt-js/tutorial/getting-started-http */
+/* Enter your code logic here. Get started with Bolt: https://slack.dev/bolt-js/tutorial/getting-started-http */
+app.command('/lovebot', async ({ command, ack, respond }) => {
+  await ack();
+  await respond({
+    "blocks": [
+      {
+        "type": "actions",
+        "elements": [
+          {
+            "type": "button",
+            "text": {
+              "type": "plain_text",
+              "text": "Click Me",
+              "emoji": true
+            },
+            "value": "love button clicked",
+            "action_id": "love-button"
+          }
+        ]
+      }
+    ]
+  });
+})
 
-  (async () => {
-    // Start your app
-    await app.start(process.env.PORT || 3000);
+(async () => {
+  // Start your app
+  await app.start(process.env.PORT || 3000);
   
-    console.log('⚡️ Bolt app is running!');
-  })();
+  console.log('⚡️ Bolt app is running!');
+})();
